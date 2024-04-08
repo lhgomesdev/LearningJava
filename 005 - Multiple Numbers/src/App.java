34b9ab23f86af56cb736fd2cd4161aa278f8c335
Luiz Henrique <lui123henri@hotmail.com>
2024-04-08 20:14:03 -0300

    att

--- 005 - Multiple Numbers/src/App.java
+++ 005 - Multiple Numbers/src/App.java
@@ -1,7 +1,21 @@
-import java.util.Scanner
+// criar um codigo que imprime se o numero é multiplo ou não
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input =  new Scanner(System.in);
+
+        System.out.println("Digite um número para encontrar seus múltiplos: ");
+        int numero = input.nextInt();
+
+        System.out.println("Digite o limite: ");
+        int limite = input.nextInt();
+
+        System.out.println("Os múltiplos de "+ numero + " até "+ limite + " são:");
+        for (int i = numero; i < limite; i += numero) {
+            System.out.println(i);
+        
+        input.close();
+        }
     }
 }
